9c3b330faad585811b33d23d2ca733ab92807b90
nicole-drml <doromal.cole@gmail.com>
2023-01-28 03:55:19 +0800

    removed comments in schema, commented code block in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,9 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         RegistrationMailer.account_created(@user).deliver
+      # else
+      #   format.html { render :new, status: :unprocessable_entity }
+      #   format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
