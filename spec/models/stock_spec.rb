1376e089a7af2f34860c8dc11e40e420d7e9f14e
nicole-drml <doromal.cole@gmail.com>
2023-01-28 08:34:52 +0800

    edited rspec

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,26 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Stock, type: :model do
-
-  client = IEX::Api::Client.new
-
-  it "creates a new stock" do
-    msft = client.quote('MSFT')
-    msft_info = client.company('MSFT')
-
-    microsoft = Stock.create(symbol: msft.symbol, name: msft_info.company_name, price: msft.latest_price)
+  context 'new stock' do
 
 
-    expect(microsoft.symbol).to eq 'MSFT'
-    expect(microsoft.name).to eq 'Microsoft Corporation'
-  end
+    it "creates a new stock" do
+      client = IEX::Api::Client.new
+      msft = client.quote('MSFT')
+      msft_info = client.company('MSFT')
+      microsoft = Stock.create(symbol: msft.symbol, name: msft_info.company_name, price: msft.latest_price)
+      
 
-  it "creates another stock" do
-    amzn = client.quote('AMZN')
-    amzn_info = client.company('AMZN')
-    amazon = Stock.create(symbol: amzn.symbol, name: amzn_info.company_name, price: amzn.latest_price)
+      expect(msft.latest_price).to_not be_nil
+      expect(msft.company_name).to_not be_nil
+      expect(microsoft.id).to_not be_nil
+    end
 
-    expect(amazon.symbol).to eq 'AMZN'
-    expect(amazon.name).to eq 'Amazon.com Inc.'
+    it "creates another stock" do
+      client = IEX::Api::Client.new
+      amzn = client.quote('AMZN')
+      amzn_info = client.company('AMZN')
+      amazon = Stock.create(symbol: amzn.symbol, name: amzn_info.company_name, price: amzn.latest_price)
+
+      expect(amzn.latest_price).to_not be_nil
+      expect(amzn_info.company_name).to_not be_nil
+      expect(amazon.id).to_not be_nil
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   fixtures :users
 
-  it "can run tests" do
-    expect(false).to be(false)
-  end
-
   it "sets the default role to trader" do
     user3 = users(:three)
     expect(user3.role).to eq 'trader'
   end
 
-  it "sets the default status to active" do
+  it "sets the default status to pending" do
     user = User.create(first_name: 'Joe', last_name: 'Terns', email: 'joe@terns.com', password: 'password')
 
     expect(user.status).to eq 'pending'
@@ -21,6 +17,6 @@ RSpec.describe User, type: :model do
   it "creates a new user" do
     user = User.create(first_name: 'Joe', last_name: 'Terns', email: 'joe@terns.com', password: 'password')
     
-    expect(user.created_at).to_not eq nil
+    expect(user.created_at).to_not be_nil
   end
 end
